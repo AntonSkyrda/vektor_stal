810801c230e029aa92c20da704bebc8f83ef56e3
AntonSkyrda <mr.skyrda@gmail.com>
2024-06-11 01:45:58 +0300

    add .gitignore add models register them on admin panel and modify settings.py

--- server/vektor_stal/admin.py
+++ server/vektor_stal/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Category,
+    SubCategory,
+    MetalCircleSizes,
+    MetalCircle,
+    MetalSheetsSizes,
+    MetalSheet,
+    MetalPipeSizes,
+    MetalPipe
+)
+
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(MetalCircleSizes)
+admin.site.register(MetalCircle)
+admin.site.register(MetalSheetsSizes)
+admin.site.register(MetalSheet)
+admin.site.register(MetalPipe)
--- server/vektor_stal/models.py
+++ server/vektor_stal/models.py
@@ -1,3 +1,96 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'subcategories'
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    price = models.CharField(max_length=255)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    steel_mark = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to="images", null=True, blank=True)
+
+
+class MetalCircleSizes(models.Model):
+    size = models.IntegerField()
+
+    class Meta:
+        ordering = ('size',)
+        verbose_name_plural = 'metal circle sizes'
+
+    def __str__(self):
+        return f"{self.size}"
+    
+
+class MetalCircle(Product):
+    sizes = models.ManyToManyField(MetalCircleSizes, related_name="metal_circles")
+
+    class Meta:
+        ordering = ("steel_mark", )
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class MetalSheetsSizes(models.Model):
+    size = models.IntegerField()
+
+    class Meta:
+        ordering = ("size", )
+        verbose_name_plural = 'metal sheets sizes'
+
+    def __str__(self):
+        return f"{self.size}"
+
+
+class MetalSheet(Product):
+    sizes = models.ManyToManyField(MetalSheetsSizes, related_name="metal_sheets")
+
+    class Meta:
+        ordering = ("steel_mark", )
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class MetalPipeSizes(models.Model):
+    size = models.IntegerField()
+
+    class Meta:
+        ordering = ("size", )
+        verbose_name_plural = 'metal pipes sizes'
+
+    def __str__(self):
+        return f"{self.size}"
+
+
+class MetalPipe(Product):
+    sizes = models.ManyToManyField(MetalPipeSizes, related_name="metal_pipes")
+
+    class Meta:
+        ordering = ("steel_mark", )
+
+    def __str__(self):
+        return f"{self.name}"
